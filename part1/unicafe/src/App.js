1dea408814fe2031fdc641d86151391f7bda05ed
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-05-05 09:47:55 -0400

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,10 +5,53 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
-  const goodJob = () => setGood(good + 1);
-  const neutralJob = () => setNeutral(neutral + 1);
-  const badJob = () => setBad(bad + 1);
+  const goodJob = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+
+    const updatedAll = updatedGood + neutral + bad;
+    setAll(updatedAll);
+
+    const updatedAverage = (updatedGood - bad) / updatedAll;
+    setAverage(updatedAverage);
+
+    const updatedPercentage = (updatedGood / updatedAll) * 100;
+
+    console.log(updatedGood / updatedAll);
+    setPercentage(updatedPercentage);
+  };
+
+  const neutralJob = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+
+    const updatedAll = updatedNeutral + good + bad;
+    setAll(updatedAll);
+
+    const updatedAverage = (good - bad) / updatedAll;
+    setAverage(updatedAverage);
+
+    const updatedPercentage = (good / updatedAll) * 100;
+    setPercentage(updatedPercentage);
+  };
+
+  const badJob = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+
+    const updatedAll = updatedBad + neutral + good;
+    setAll(updatedAll);
+
+    const updatedAverage = (good - updatedBad) / updatedAll;
+    setAverage(updatedAverage);
+
+    const updatedPercentage = (good / updatedAll) * 100;
+    setPercentage(updatedPercentage);
+  };
 
   return (
     <div>
@@ -28,6 +71,15 @@ const App = () => {
       </div>
       <div>
         <strong>bad</strong> {bad}
+      </div>
+      <div>
+        <strong>all</strong> {all}
+      </div>
+      <div>
+        <strong>average</strong> {average}
+      </div>
+      <div>
+        <strong>positive</strong> {percentage} %
       </div>
     </div>
   );
