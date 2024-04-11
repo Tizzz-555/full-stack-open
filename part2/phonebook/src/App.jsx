2aa3cd7191f4c0766555f4a35e32f0bccf1f8165
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-04-11 18:35:35 +0200

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ newFilter, handleFilter }) => {
 	return (
@@ -53,6 +54,14 @@ const App = () => {
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [newFilter, setNewFilter] = useState("");
+
+	const hook = () => {
+		axios.get("http://localhost:3001/persons").then((response) => {
+			setPersons(response.data);
+		});
+	};
+
+	useEffect(hook, []);
 
 	const handleNameInputChange = (event) => {
 		setNewName(event.target.value);
