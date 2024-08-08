9135817bc1d55a0a4bafbc1876f37245bf9b64e0
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-08-08 19:14:09 +0200

    5.7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+	const [detailsVisible, setDetailsVisible] = useState(false);
+	console.log(blog);
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+	const showWhenVisible = { display: detailsVisible ? "" : "none" };
+
+	return (
+		<div style={blogStyle}>
+			<div id="header">
+				{blog.title} {blog.author}
+				<button onClick={() => setDetailsVisible(!detailsVisible)}>
+					{detailsVisible ? "Hide" : "View"}
+				</button>
+			</div>
+			<div id="details" style={showWhenVisible}>
+				<div>{blog.url}</div>
+				<div>
+					likes: {blog.likes}
+					<button>Like</button>
+				</div>
+				<div>{blog.user.username}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
