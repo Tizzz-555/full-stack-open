063b2116d0bd49ebda1da29624c8494e7ec2126f
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-08-08 18:11:44 +0200

    5.6

---
+++ part5/bloglist/src/components/BlogForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+	const [newTitle, setNewTitle] = useState("");
+	const [newAuthor, setNewAuthor] = useState("");
+	const [newUrl, setNewUrl] = useState("");
+
+	const addBlog = (e) => {
+		e.preventDefault();
+		createBlog({ title: newTitle, author: newAuthor, url: newUrl });
+
+		setNewTitle("");
+		setNewAuthor("");
+		setNewUrl("");
+	};
+
+	return (
+		<div>
+			<h2>Create new</h2>
+			<form onSubmit={addBlog}>
+				<div>
+					Title:{" "}
+					<input
+						value={newTitle}
+						onChange={(e) => setNewTitle(e.target.value)}
+					/>
+				</div>
+				<div>
+					Author:{" "}
+					<input
+						value={newAuthor}
+						onChange={(e) => setNewAuthor(e.target.value)}
+					/>
+				</div>
+				<div>
+					Url:{" "}
+					<input value={newUrl} onChange={(e) => setNewUrl(e.target.value)} />
+				</div>
+				<button type="submit">Create</button>
+			</form>
+		</div>
+	);
+};
+
+export default BlogForm;
