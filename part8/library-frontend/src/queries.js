abc274b7c0da39f059a816470571d30b1a70d0ca
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-02-05 19:34:05 +0100

    8.8

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+// export const ALL_BOOKS = gql`
+//   query {
+//     allBooks {
+
+//     }
+//   }
+// `
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
