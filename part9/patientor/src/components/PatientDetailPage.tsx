4f968b33f5d8b17e5129c3c831fb1dd185f73603
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-05-08 18:58:25 +0200

    9.23

---
+++ part9/patientor/src/components/PatientDetailPage.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import patientService from "../services/patients";
+import { Patient } from "../types";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+const PatientDetailPage = () => {
+	const [patient, setPatient] = useState<Patient>();
+	const { id } = useParams<{ id: string }>();
+
+	useEffect(() => {
+		if (!id) return;
+		const fetchPatient = async () => {
+			const patient = await patientService.getPatient(id);
+			setPatient(patient);
+		};
+		void fetchPatient();
+	}, [id]);
+	return (
+		<div>
+			<Box sx={{ margin: "1em 0" }}>
+				<Typography variant="h4" display="inline" sx={{ marginRight: "0.5em" }}>
+					{patient?.name}
+					{patient?.gender === "male" ? (
+						<MaleIcon fontSize="large" />
+					) : (
+						<FemaleIcon fontSize="large" />
+					)}
+				</Typography>
+				<Typography sx={{ marginTop: "1em" }}>ssn: {patient?.ssn}</Typography>
+				<Typography>occupation: {patient?.occupation}</Typography>
+			</Box>
+		</div>
+	);
+};
+
+export default PatientDetailPage;
